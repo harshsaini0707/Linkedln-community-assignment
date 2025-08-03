b30b175e2715a12d42e48fd5f51c664c64349598
harshsaini0707 <harshsaini0172@gmail.com>
2025-08-03 18:06:32 +0530

    deployment-fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-import "./src/env.js";
-
 /** @type {import("next").NextConfig} */
 const config = {
-    eslint:{
-        ignoreDuringBuilds : true
-    },
-    typescript:{
-        ignoreBuildErrors:true
-    },
-     output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'standalone',
 };
 
 export default config;
-
-
-
