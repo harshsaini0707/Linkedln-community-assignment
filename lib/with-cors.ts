ce3ed84fff36f0f7d200b41d213c5ddded4ef97b
harshsaini0707 <harshsaini0172@gmail.com>
2025-08-03 20:04:49 +0530

    error-fixx

--- lib/with-cors.ts
+++ lib/with-cors.ts
@@ -1,14 +1,13 @@
+import { NextResponse } from "next/server";
 
-import { NextResponse, NextRequest } from "next/server";
 const allowedOrigins = [
   "https://linkedln-community-assignment.vercel.app",
   "https://linkedln-community-assignment-1r80cg9zl-harsh-sainis-projects.vercel.app"
 ];
 
 export function withCORS(handler: (req: Request) => Promise<NextResponse>) {
-  return async (req: Request): Promise<NextResponse> => {
+  return async (req: Request) => {
     const origin = req.headers.get("origin") || "";
-
 
     if (req.method === "OPTIONS") {
       const res = new NextResponse(null, { status: 204 });
@@ -27,8 +26,8 @@ function applyCORSHeaders(res: NextResponse, origin: string) {
     res.headers.set("Access-Control-Allow-Origin", origin);
   }
 
-  res.headers.set("Vary", "Origin"); 
   res.headers.set("Access-Control-Allow-Credentials", "true");
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.headers.set("Vary", "Origin");
 }
